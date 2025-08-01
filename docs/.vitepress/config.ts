b78b5f98b8344d1c3bce487c09844d7cd9716d48
Dj <177370576+DJSng4@users.noreply.github.com>
2025-08-01 14:07:00 -0300

    try and change "on this page" with different quotes

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -51,13 +51,5 @@ export default defineConfig({
 })
 
 interface Outline {
-  /**
-   * The levels of headings to be displayed in the outline.
-   * Single number means only headings of that level will be displayed.
-   * If a tuple is passed, the first number is the minimum level and the second number is the maximum level.
-   * `'deep'` is same as `[2, 6]`, which means all headings from `<h2>` to `<h6>` will be displayed.
-   *
-   * @default 1
-   */
-  label?: 'Table of Contents'
+  label?: "Table of Contents"
 }
