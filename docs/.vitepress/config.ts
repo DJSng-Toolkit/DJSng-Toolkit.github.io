310fe76ca25984b7fd286340c41c32e113eaca2a
Dj <177370576+DJSng4@users.noreply.github.com>
2025-08-01 13:54:38 -0300

    this should make the aisle nest i think

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -48,3 +48,17 @@ export default defineConfig({
     ]
   }
 })
+
+interface Outline {
+  /**
+   * The levels of headings to be displayed in the outline.
+   * Single number means only headings of that level will be displayed.
+   * If a tuple is passed, the first number is the minimum level and the second number is the maximum level.
+   * `'deep'` is same as `[2, 6]`, which means all headings from `<h2>` to `<h6>` will be displayed.
+   *
+   * @default 1
+   */
+  level?: 'deep'
+
+  label?: Table of Contents
+}
